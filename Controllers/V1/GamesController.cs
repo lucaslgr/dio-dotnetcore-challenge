275b60651564a44e815f51dc37e04d66db8c2366
lucaslgr <lucaslgr1206@gmail.com>
2021-07-06 20:12:18 -0300

    Implemented the GameInputModel and GameViewModel; close #2

--- Controllers/V1/GamesController.cs
+++ Controllers/V1/GamesController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using ApiGames.InputModel;
+using ApiGames.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiGames.Controllers.V1
@@ -10,37 +12,37 @@ namespace ApiGames.Controllers.V1
     public class GamesController: ControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<List<object>>> Get()
+        public async Task<ActionResult<List<GameViewModel>>> Get()
         {
             return Ok();
         }
 
         [HttpGet("{gameID:guid}")]
-        public async Task<ActionResult<object>> Get(Guid gameID)
+        public async Task<ActionResult<GameViewModel>> Get(Guid gameID)
         {
             return Ok();
         }
 
         [HttpPost]
-        public async Task<ActionResult<object>> Insert(object game)
+        public async Task<ActionResult<GameViewModel>> Insert(GameInputModel game)
         {
             return Ok();
         }
 
         [HttpPut("{gameID:guid}")]
-        public async Task<ActionResult<object>> Update(Guid gameID, object game)
+        public async Task<ActionResult<GameViewModel>> Update(Guid gameID, GameInputModel game)
         {
             return Ok();
         }
 
         [HttpPatch("{gameID:guid}/price/{price:double}")]
-        public async Task<ActionResult<object>> Update(Guid gameID, double price)
+        public async Task<ActionResult<GameViewModel>> Update(Guid gameID, double price)
         {
             return Ok();
         }
 
         [HttpDelete("{gameID:guid}")]
-        public async Task<ActionResult<object>> Delete(Guid gameID)
+        public async Task<ActionResult<GameViewModel>> Delete(Guid gameID)
         {
             return Ok();
         }
---
+++ ViewModel/GameViewModel.cs
@@ -0,0 +1,10 @@
+namespace ApiGames.ViewModel
+{
+    public class GameViewModel
+    {
+        public Guid Id {get; set;}
+        public string Name {get; set;}
+        public string Producer{get; set;}
+        public double Price{get; set;}
+    }
+}
