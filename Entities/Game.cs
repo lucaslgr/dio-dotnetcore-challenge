43eae89e094f4f027ad488084910f19465b794c3
lucaslgr <lucaslgr1206@gmail.com>
2021-07-07 08:48:24 -0300

    Implemented the Game Entity; close #7

---
+++ Entities/Game.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ApiGames.Entities
+{
+    public class Game
+    {
+        public Guid Id {get; set;}
+        public string Name {get; set;}
+        public string Producer{get; set;}
+        public double Price{get; set;}
+    }
+}
